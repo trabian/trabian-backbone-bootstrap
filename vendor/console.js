30dee6641c445d5bfbfb0505d0db96e95ca7d2ec
Matt Dean <matt@trabian.com>
2012-05-29 16:17:05 -0400

    Dont override console.log on Chrome

--- vendor/console.js
+++ vendor/console.js
@@ -1,4 +1,4 @@
-if (!window.console || !console.firebug)
+if (!window.console)
 {
     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
